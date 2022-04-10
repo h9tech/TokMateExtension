1756cd8ccaf4a608ed2c464d92f70b17de942086
Utsav Karki <Utsavkarki244@gmail.com>
2022-04-10 13:15:01 +0545

    Change popup page

--- js/popup.js
+++ js/popup.js
@@ -1,44 +1,29 @@
-// https://github.com/tkrisztian95/netflix-imdb-chrome-extension
+const downloadToggler = document.getElementById("downloadToggler");
+const downloadArea = document.getElementById("download-area");
 
-const LOCAL_STORAGE_API_KEY = 'apikey';
-let apiKey = '';
+downloadArea.style.display = "none";
 
-chrome.storage.sync.get(LOCAL_STORAGE_API_KEY, (result) => {
+downloadToggler.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    downloadArea.style.display = "block";
 
-    apiKey = result[LOCAL_STORAGE_API_KEY];
-    console.log('Api key loaded:' + apiKey);
+    return;
+  }
+
+  downloadArea.style.display = "none";
 });
 
-function saveApiKey() {
-    const apiKey = document.getElementById('apikey').value;
-    const jsonFile = {};
-    jsonFile[LOCAL_STORAGE_API_KEY] = apiKey;
-    chrome.storage.sync.set(jsonFile);
-    document.getElementById('display_message').style.display = '';
-    console.log(`Token updated: ${JSON.stringify(jsonFile)}`);
-}
+const telegramToggler = document.getElementById("telegramToggler");
+const telegramArea = document.getElementById("telegram-area");
 
-document.getElementById('btnSetApiKey').addEventListener('click', saveApiKey);
-chrome.storage.sync.get(LOCAL_STORAGE_API_KEY, (result) => {
-    document.getElementById('apikey').value = result[LOCAL_STORAGE_API_KEY];
-});
+telegramArea.style.display = "none";
 
-window.addEventListener('click', (e) => {
-    if (e.target.href !== undefined) {
-        chrome.tabs.create({ url: e.target.href });
-    }
-});
+telegramToggler.addEventListener("change", (a) => {
+  if (a.target.checked) {
+    telegramArea.style.display = "block";
+  
+    return;
+  }
 
-chrome.storage.onChanged.addListener((changes, namespace) => {
-    for (let key in changes) {
-        const storageChange = changes[key];
-        if (key === LOCAL_STORAGE_API_KEY) {
-            console.log('Token updated');
-            apiKey = storageChange.newValue;
-        }
-        console.log(
-            `Storage key "${key}" in namespace "${namespace}" changed. 
-            Old value was "${storageChange.oldValue}", new value is "${storageChange.newValue}".`
-        );
-    }
+  telegramArea.style.display = "none";
 });
